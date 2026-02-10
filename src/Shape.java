3ef14497a4cfb1f66abb1aee3e6104736ac7826f
EscapedLizard <bethanymixon101@gmail.com>
2026-02-10 14:42:35 -0500

    Creates Shape interface and Implement a Circle and Rectangle class

---
+++ src/Shape.java
@@ -0,0 +1,11 @@
+public interface Shape {
+    /**
+     * Return the area of the shape.
+     */
+    double area();
+
+    /**
+     * Return the perimeter (or circumference) of the shape.
+     */
+    double perimeter();
+}
