22386749b522670f5f486ddff2feefa8c7c08649
EscapedLizard <bethanymixon101@gmail.com>
2026-02-11 17:37:27 -0500

    Completed base project

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main (String[] args){
 
-        System.out.printf("Circle(2.5): area =%.3f perimeter = %.3f", new Circle(2.5).area(), new Circle(2.5).perimeter());
+        System.out.printf("Circle(2.5): area =%.3f perimeter = %.3f", new Circle(3.6).area(), new Circle(2.5).perimeter());
+        System.out.println();
         System.out.printf("Rectangle(2,5): area =%.3f, perimeter =%.3f", new Rectangle(2,5).area(), new Rectangle(2,5).perimeter());
+        System.out.println();
         System.out.printf("Triangle(3,5,7): area =%.3f, perimeter =%.3f", new Triangle(2,3,5).area(), new Triangle(2,3,5).perimeter());
 
     }
